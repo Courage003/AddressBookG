e4af08521b337b440730e2089aefb14cca602f88
Courage003 <rastogidhruv28@gmail.com>
2025-01-03 19:07:58 +0530

    Implementation of multiple contact feature added

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-//First UC1 feature added
+
         System.out.println("Welcome to Address Book Program!");
         AddressBook addressBook= new AddressBook();
         Scanner sc = new Scanner(System.in);
@@ -22,31 +22,37 @@ public class Main {
 
             switch (choice) {
                 case 1:
-                    //Add a new contact
-                    System.out.println("Enter contact details: ");
-                    System.out.println("Enter contact details: ");
-                    System.out.print("First Name: ");
-                    String firstName = sc.nextLine();
-                    System.out.print("Last Name: ");
-                    String lastName = sc.nextLine();
-                    System.out.print("Address: ");
-                    String address = sc.nextLine();
-                    System.out.print("City: ");
-                    String city = sc.nextLine();
-                    System.out.print("State: ");
-                    String state = sc.nextLine();
-                    System.out.print("Zip: ");
-                    String zip = sc.nextLine();
-                    System.out.print("Phone Number: ");
-                    String phoneNumber = sc.nextLine();
-                    System.out.print("Email: ");
-                    String email = sc.nextLine();
+                    //Add multiple contacts
+                    boolean addMore = true;
+                    while(addMore){
+                        System.out.println("Enter contact details: ");
+                        System.out.print("First Name: ");
+                        String firstName = sc.nextLine();
+                        System.out.print("Last Name: ");
+                        String lastName = sc.nextLine();
+                        System.out.print("Address: ");
+                        String address = sc.nextLine();
+                        System.out.print("City: ");
+                        String city = sc.nextLine();
+                        System.out.print("State: ");
+                        String state = sc.nextLine();
+                        System.out.print("Zip: ");
+                        String zip = sc.nextLine();
+                        System.out.print("Phone Number: ");
+                        String phoneNumber = sc.nextLine();
+                        System.out.print("Email: ");
+                        String email = sc.nextLine();
 
-                    Contact contact = new Contact(firstName, lastName, address, city, state, zip, phoneNumber, email);
-                    addressBook.addContact(contact);
+                        Contact contact = new Contact(firstName, lastName, address, city, state, zip, phoneNumber, email);
+                        addressBook.addContact(contact);
 
-                    System.out.println("Contact added successfully!");
-                    addressBook.displayContacts();
+                        System.out.println("\nDo you want to add another contact?: ");
+                        String choiceAdd = sc.nextLine();
+                        addMore = choiceAdd.equalsIgnoreCase("yes");
+
+
+                    }
+
                     break;
 
                 case 2:
